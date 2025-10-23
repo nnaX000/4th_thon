6069be4ed78b210dec08507da3408bbb706ef559
김나영 <kny200208@naver.com>
2025-10-23 21:01:15 +0900

    fix : swagger 시큐리티 허용

--- src/main/java/com/example/fourth/config/SecurityConfig.java
+++ src/main/java/com/example/fourth/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                     return config;
                 }))
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/user/**").permitAll()
+                        .requestMatchers("/api/user/**","/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess
--- src/main/java/com/example/fourth/service/UserService.java
+++ src/main/java/com/example/fourth/service/UserService.java
@@ -44,4 +44,6 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 
+    //로그인
+
 }
