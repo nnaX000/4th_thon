9e2b13a137394b93b301ae5ed7e1d0c15ae2cf12
baseballclub <abkimdoyo25@gmail.com>
2025-10-28 01:37:54 +0900

    Get Folder list By user and Get Reports By Folder

--- src/main/java/com/example/fourth/dto/ReportFolderResponse.java
+++ src/main/java/com/example/fourth/dto/ReportFolderResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @AllArgsConstructor
 @Data
-public class FolderResponse {
+public class ReportFolderResponse {
     private Long id;
     private String name;
 }
--- src/main/java/com/example/fourth/service/ReportFolderService.java
+++ src/main/java/com/example/fourth/service/ReportFolderService.java
@@ -1,7 +1,7 @@
 package com.example.fourth.service;
 
 
-import com.example.fourth.dto.FolderResponse;
+import com.example.fourth.dto.ReportFolderResponse;
 import com.example.fourth.entity.Folder;
 import com.example.fourth.entity.Report;
 import com.example.fourth.entity.User;
@@ -9,7 +9,9 @@ import com.example.fourth.repository.FolderRepository;
 import com.example.fourth.repository.ReportRepository;
 import com.example.fourth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -25,11 +27,11 @@ public class ReportFolderService {
 
         // 리포트 찾기
         Report report = reportRepository.findById(reportId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 리포트를 찾을 수 없습니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 리포트를 찾을 수 없습니다."));
 
         // 사용자 조회
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자 입니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."));
 
         // 폴더 존재 여부 확인
         Folder folder = folderRepository.findByNameAndUser_Id(folderName, userId)
@@ -46,10 +48,23 @@ public class ReportFolderService {
         return reportRepository.save(report);
     }
 
-    public List<FolderResponse> getFolderByUser(int userId) {
+    // 유저가 가진 폴더 리스트 띄우기
+    public List<ReportFolderResponse> getFolderByUser(int userId) {
         List<Folder> folders = folderRepository.findByUser_Id(userId);
         return folders.stream()
-                .map(folder -> new FolderResponse(folder.getId(), folder.getName()))
+                .map(folder -> new ReportFolderResponse(folder.getId(), folder.getName()))
+                .toList();
+    }
+
+    // 폴더 안의 리포트들 띄우기
+    public List<ReportFolderResponse> getReportsByFolder(Long folderId) {
+        Folder folder = folderRepository.findById(folderId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 폴더입니다."));
+
+        List<Report> reports = reportRepository.findByFolder_Id(folderId);
+
+        return reports.stream()
+                .map(report -> new ReportFolderResponse(report.getId(), report.getTitle()))
                 .toList();
     }
 }
